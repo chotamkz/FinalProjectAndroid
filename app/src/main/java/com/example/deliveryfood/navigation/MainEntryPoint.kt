6ed7195612cb3fa376919a761f64e5ee84b17522
Daniyar Nurlanov <108024213+chotamkz@users.noreply.github.com>
2023-12-23 14:33:10 +0600

    main

---
+++ app/src/main/java/com/example/deliveryfood/navigation/MainEntryPoint.kt
@@ -0,0 +1,90 @@
+package com.example.deliveryfood.navigation
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.deliveryfood.viewmodels.CartViewModel
+import com.example.deliveryfood.viewmodels.MenuViewModel
+import com.example.deliveryfood.viewmodels.ProfileViewModel
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+
+    Scaffold(
+        bottomBar = {
+            BottomNavigation(navController = navController)
+        }
+    ) { innerPadding ->
+        Box(modifier = Modifier.padding(innerPadding)) {
+            NavGraph(
+                navController = navController
+            )
+        }
+    }
+}
+
+@Composable
+fun BottomNavigation(navController: NavHostController) {
+    val screens = listOf(
+        BottomBarScreen.Menu,
+        BottomBarScreen.Profile,
+        BottomBarScreen.Cart
+    )
+    val newBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = newBackStackEntry?.destination
+
+    BottomNavigation(
+        backgroundColor = Color.LightGray
+    ) {
+        screens.forEach { screen ->
+            AddItem(
+                screen = screen,
+                currentDestination = currentDestination,
+                navController = navController
+            )
+        }
+    }
+}
+
+@Composable
+fun RowScope.AddItem(
+    screen: BottomBarScreen,
+    currentDestination: NavDestination?,
+    navController: NavHostController
+) {
+    BottomNavigationItem(
+        label = {
+            Text(text = screen.title)
+        },
+        icon = {
+            Icon(
+                painterResource(id = screen.icon),
+                contentDescription = "Navigation Icon"
+            )
+        },
+        selected = currentDestination?.hierarchy?.any {
+            it.route == screen.route
+        } == true,
+        onClick = {
+            navController.navigate(screen.route) {
+                popUpTo(navController.graph.findStartDestination().id)
+                launchSingleTop = true
+            }
+        }
+    )
+}
